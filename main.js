2156b437548791302209a6d5461d17c50381137d
Muneeba-Nurudeen <nurudeenmuneeba99@gmail.com>
2022-03-14 13:39:39 +0000

    addad some data

--- main.js
+++ main.js
@@ -29,37 +29,114 @@ const addTodo = () => {
         
                 todoInput.parentElement.classList.add("error")
             }
-            todoInput.value = ''
+            todoInput.value = "";
         } else {
-            alert('you have five things to do')
+            alert("you have five things to do")
         }
-    }
+    };
 
         
         //event handlers
-         addBtn.addEventListener('click', addTodo)
+         addBtn.addEventListener("click", addTodo);
 
 const setTodo = (arr) => {
-    let htmlArr ='';
+    let htmlArr = "";
 
 for(el of arr) {
         let html =
         `<li id=${el}>
-
     <div class ="todo">
      <p>${el}</p>
     <input type="text" class="hide"/>
     </div>
        
-      < div class ="btns">
+      <div class ="btns">
      <button>Edit</button>
      <button>Save</button>
      <button>Delete</button>
       </div>
       
-      </li>`
+      </li>`;
       htmlArr += html
     }
     todoList.innerHTML =htmlArr
 //informText()
 }
+
+
+//sign in functions
+const signIn = document.querySelector(".logo-wrap p")
+const modal= document.querySelector("#modal")
+const userNm = document.querySelector("input")
+const signBtn = document.querySelector("button")
+const disModal = document.querySelector("span")
+
+signIn.addEventListener("click", () =>{
+    modal.style.display = "flex"
+})
+disModal.addEventListener("click", (e) =>{
+    modal.style.display = "none"
+})
+modal.addEventListener("click", (e) =>{
+    e.target==modal? modal.style.display ="none" : null
+})
+//function to log in user and save todo list
+signBtn.addEventListener("click", () =>{
+    user = userNm.value
+    if (user ==""){
+        userNm.classList.add("error")
+    } else{
+        userNm.classList.remove("error")
+        signIn.innerText = user        //changing the "sign in" text to username
+        todoArray.clear()
+        setTodo(todoArray)
+        userNm.value = ""
+
+let db = [...todoArray]
+let _db =(JSON.stringify(db))
+let userData = localStorage.getItem(user) 
+
+if(userData){
+    let _userData =JSON.parse(userData)
+    todoArray = new Set(_userData)
+    setTodo(todoArray)
+    modal.style.display ="none"
+}else{
+    localStorage.setItem(user, _db)
+    modal.style.display = "none"
+}
+
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
